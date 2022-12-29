a7a674a8b090e967694e0c8ff330997d23347c9b
Dyanesh Dhawale <dyanixdhawale@gmail.com>
2022-12-29 23:24:47 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Typography } from '@material-ui/core';
+import Particles from './components/Particles';
 
-function App() {
+
+const useStyles = makeStyles((theme)=>({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+    },
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+const App = () => {
+  const classes =useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className={classes.wrapper}> 
+    
+    <AppBar  className={classes.appBar} position="static" color='inherit'>
+      <Typography variant="h2" align="center">Vodeo</Typography>
+      
+    </AppBar>
+        <VideoPlayer />
+        <Options>
+        <Notifications />
+        </Options>    
+        <Particles />
     </div>
-  );
+
+  )
 }
 
 export default App;
+
+
+
